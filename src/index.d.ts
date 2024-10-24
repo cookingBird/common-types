af2a2f2411fd09b5c584ba7a2f520d09a5e40c8c
Deng Tao <d17381844023@gmail.com>
2024-10-24 10:50:01 +0800

    feat: add index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,16 @@
+export type IsEqual<A, B> = (<T>() => T extends A ? 1 : 2) extends <T>() => T extends B
+  ? 1
+  : 2
+  ? true
+  : false;
+
+export type Shift<T extends any[]> = ((...t: T) => any) extends (
+  first: any,
+  ...rest: infer Rest
+) => any
+  ? Rest
+  : never;
+
+export type NOOP = () => void;
+
+export type ANYOP = (...params: any[]) => any | void;
